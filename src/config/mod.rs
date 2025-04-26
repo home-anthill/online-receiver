cc7cfe51c66e0052ed7e059ffd9b45c2e85aed8d
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2025-04-26 23:55:10 +0200

    fix: skip log initialization while running tests
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,6 +1,8 @@
+use std::env;
+
 use dotenvy::dotenv;
 use serde::Deserialize;
-use tracing::{Metadata, info};
+use tracing::info;
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_subscriber::fmt::writer::MakeWriterExt;
 
@@ -19,44 +21,38 @@ pub struct Env {
     pub mqtt_key_file: String,
 }
 
-fn filter_target(meta: &Metadata) -> bool {
-    meta.target() == "app"
-}
-
-fn skip_filter_target(_meta: &Metadata) -> bool {
-    true
-}
-
 pub fn init() -> Env {
     // Load the .env file
     dotenv().ok();
     let env = envy::from_env::<Env>().ok().unwrap();
 
     // Configure logging
-    let stdout = std::io::stdout;
-    let debug_file = RollingFileAppender::builder()
-        .rotation(Rotation::DAILY)
-        .filename_prefix("all")
-        .filename_suffix("log")
-        .max_log_files(5)
-        .build("./logs")
-        .expect("initializing rolling debug_file appender failed")
-        .with_filter(|meta| meta.target() == "app");
-    let error_file = RollingFileAppender::builder()
-        .rotation(Rotation::DAILY)
-        .filename_prefix("error")
-        .filename_suffix("log")
-        .max_log_files(5)
-        .build("./logs")
-        .expect("initializing rolling error_file appender failed")
-        .with_filter(|meta| meta.target() == "app")
-        .with_max_level(tracing::Level::ERROR);
-    let writer = debug_file.and(error_file).and(stdout);
-    tracing_subscriber::fmt()
-        .compact()
-        .with_writer(writer)
-        .with_ansi(false)
-        .init();
+    if env::var("ENV") != Ok("testing".to_string()) {
+        let stdout = std::io::stdout;
+        let debug_file = RollingFileAppender::builder()
+            .rotation(Rotation::DAILY)
+            .filename_prefix("all")
+            .filename_suffix("log")
+            .max_log_files(5)
+            .build("./logs")
+            .expect("initializing rolling debug_file appender failed")
+            .with_filter(|meta| meta.target() == "app");
+        let error_file = RollingFileAppender::builder()
+            .rotation(Rotation::DAILY)
+            .filename_prefix("error")
+            .filename_suffix("log")
+            .max_log_files(5)
+            .build("./logs")
+            .expect("initializing rolling error_file appender failed")
+            .with_filter(|meta| meta.target() == "app")
+            .with_max_level(tracing::Level::ERROR);
+        let writer = debug_file.and(error_file).and(stdout);
+        tracing_subscriber::fmt()
+            .compact()
+            .with_writer(writer)
+            .with_ansi(false)
+            .init();
+    }
 
     info!(target: "app", "Starting application...");
 
